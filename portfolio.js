1321487917bf31d4e0b8dc63a74e0155971ce429
veaucaro <127398031+veaucaro@users.noreply.github.com>
2023-09-24 19:57:31 +0200

    clear screen

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,19 @@
 // portfolio.js
-const readline = require('readline');
+const readline = require('readline'); // ask for input
+const clear = require('clear'); // clear screen
 
+//define input / output
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+// Function to clear the screen
+function clearScreen() {
+    clear();
+  }
+  
+  clearScreen();
 
 rl.question('Quel est votre prénom ? ', (prenom) => {
   console.log(`Joli prénom, ${prenom} !`);
